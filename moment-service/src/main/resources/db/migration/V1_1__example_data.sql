811e9bfff29dc70cca31944a357ec70f0801261e
MichaelJG.Kopp <michaeljg.kopp@gmail.com>
2025-05-13 10:16:52 +0200

    add sample unsplash image urls

--- moment-service/src/main/resources/db/migration/V1_1__example_data.sql
+++ moment-service/src/main/resources/db/migration/V1_1__example_data.sql
@@ -13,11 +13,11 @@
            ('San Francisco', '101 Market St');
 
     -- Insert into moments
-    INSERT INTO moments (host_id, category_id, location_id, title, short_description, start_date, recurrence, price, status, ticket_count)
-    VALUES (1, 1, 1, 'Concert in the Park', 'A live music concert in the park.', '2025-06-01 19:00:00', 'ONETIME', 50.00, 'LIVE', 500),
-           (2, 2, 2, 'Art Exhibition', 'An art exhibition showcasing modern art.', '2025-07-10 18:00:00', 'REGULAR', 25.00, 'LIVE', 200),
-           (3, 3, 3, 'Comedy Show', 'A stand-up comedy show with popular comedians.', '2025-08-15 20:00:00', 'ONETIME', 30.00, 'DRAFT', 150),
-           (4, 4, 4, 'Food Festival', 'A food festival with various cuisines.', '2025-09-05 10:00:00', 'REGULAR', 15.00, 'LIVE', 800);
+    INSERT INTO moments (host_id, category_id, location_id, title, short_description, start_date, recurrence, price, status, ticket_count, thumbnail)
+    VALUES (1, 1, 1, 'Concert in the Park', 'A live music concert in the park.', '2025-06-01 19:00:00', 'ONETIME', 50.00, 'LIVE', 500, 'https://unsplash.com/photos/person-performing-heart-hand-gesture-hzgs56Ze49s'),
+           (2, 2, 2, 'Art Exhibition', 'An art exhibition showcasing modern art.', '2025-07-10 18:00:00', 'REGULAR', 25.00, 'LIVE', 200, 'https://unsplash.com/photos/person-performing-heart-hand-gesture-hzgs56Ze49s'),
+           (3, 3, 3, 'Comedy Show', 'A stand-up comedy show with popular comedians.', '2025-08-15 20:00:00', 'ONETIME', 30.00, 'DRAFT', 150, 'https://unsplash.com/photos/person-performing-heart-hand-gesture-hzgs56Ze49s'),
+           (4, 4, 4, 'Food Festival', 'A food festival with various cuisines.', '2025-09-05 10:00:00', 'REGULAR', 15.00, 'LIVE', 800, 'https://unsplash.com/photos/person-performing-heart-hand-gesture-hzgs56Ze49s');
 
     -- Insert into moment_details
     INSERT INTO moment_details (moment_id, description)
