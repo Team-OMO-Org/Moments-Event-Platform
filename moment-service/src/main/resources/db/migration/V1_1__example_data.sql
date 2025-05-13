27fcb441d81fe847957ffaf8b5444c83713f1a3c
MichaelJG.Kopp <michaeljg.kopp@gmail.com>
2025-05-13 11:12:01 +0200

    refactor into product fragment and add placeholder iamge urls

--- moment-service/src/main/resources/db/migration/V1_1__example_data.sql
+++ moment-service/src/main/resources/db/migration/V1_1__example_data.sql
@@ -15,7 +15,7 @@
     -- Insert into moments
     INSERT INTO moments (host_id, category_id, location_id, title, short_description, start_date, recurrence, price, status, ticket_count, thumbnail)
     VALUES (1, 1, 1, 'Concert in the Park', 'A live music concert in the park.', '2025-06-01 19:00:00', 'ONETIME', 50.00, 'LIVE', 500, 'https://unsplash.com/photos/person-performing-heart-hand-gesture-hzgs56Ze49s'),
-           (2, 2, 2, 'Art Exhibition', 'An art exhibition showcasing modern art.', '2025-07-10 18:00:00', 'REGULAR', 25.00, 'LIVE', 200, 'https://unsplash.com/photos/person-performing-heart-hand-gesture-hzgs56Ze49s'),
+           (2, 2, 2, 'Art Exhibition', 'An art exhibition showcasing modern art.', '2025-07-10 18:00:00', 'REGULAR', 25.00, 'LIVE', 200, 'https://unsplash.com/photos/people-partying-with-confetti-ZODcBkEohk8?utm_content=creditShareLink&utm_medium=referral&utm_source=unsplash'),
            (3, 3, 3, 'Comedy Show', 'A stand-up comedy show with popular comedians.', '2025-08-15 20:00:00', 'ONETIME', 30.00, 'DRAFT', 150, 'https://unsplash.com/photos/person-performing-heart-hand-gesture-hzgs56Ze49s'),
            (4, 4, 4, 'Food Festival', 'A food festival with various cuisines.', '2025-09-05 10:00:00', 'REGULAR', 15.00, 'LIVE', 800, 'https://unsplash.com/photos/person-performing-heart-hand-gesture-hzgs56Ze49s');
 
