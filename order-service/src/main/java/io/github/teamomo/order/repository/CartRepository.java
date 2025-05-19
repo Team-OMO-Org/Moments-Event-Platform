8630b4feb8dd39e8b55c1e524afd485ae9d2792d
OlenaShcherbina <olenas.shcherbina@gmail.com>
2025-05-19 16:01:00 +0200

    add create and update Cart without updating List of Items

--- order-service/src/main/java/io/github/teamomo/order/repository/CartRepository.java
+++ order-service/src/main/java/io/github/teamomo/order/repository/CartRepository.java
@@ -1,6 +1,9 @@
 package io.github.teamomo.order.repository;
 
 import io.github.teamomo.order.entity.Cart;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface CartRepository extends JpaRepository<Cart, Long> {}
+public interface CartRepository extends JpaRepository<Cart, Long> {
+  Optional<Cart> findByCustomerId(Long customerId);
+}
--- order-service/src/main/java/io/github/teamomo/order/service/OrderService.java
+++ order-service/src/main/java/io/github/teamomo/order/service/OrderService.java
@@ -1,6 +1,10 @@
 package io.github.teamomo.order.service;
 
+import io.github.teamomo.moment.exception.ResourceNotFoundException;
 import io.github.teamomo.order.dto.CartDto;
+import io.github.teamomo.order.entity.Cart;
+import io.github.teamomo.order.exception.ResourceAlreadyExistsException;
+import io.github.teamomo.order.mapper.OrderMapper;
 import io.github.teamomo.order.repository.CartItemRepository;
 import io.github.teamomo.order.repository.CartRepository;
 import io.github.teamomo.order.repository.OrderItemRepository;
@@ -18,6 +22,42 @@ public class OrderService {
   private final OrderRepository orderRepository;
   private final OrderItemRepository orderItemRepository;
   private final PaymentRepository paymentRepository;
+  private final OrderMapper orderMapper;
 
 
+  public CartDto findCartByCustomerId(Long customerId) {
+    Cart cart = cartRepository
+        .findByCustomerId(customerId)
+        .orElseGet(() -> {
+          Cart newCart = new Cart();
+          newCart.setCustomerId(customerId);
+          return newCart;
+        });
+    return orderMapper.toCartDto(cart);
+  }
+
+//todo: create items
+  public CartDto createCart(CartDto cartDto) {
+    if(cartRepository.findByCustomerId(cartDto.customerId()).isPresent()){
+      throw new ResourceAlreadyExistsException("Cart already exist with given customer ID " + cartDto.customerId());
+    }
+    Cart cart = orderMapper.toCartEntity(cartDto);
+    Cart savedCart = cartRepository.save(cart);
+
+    return orderMapper.toCartDto(savedCart);
+  }
+
+//todo: update items
+  public CartDto updateCart(Long customerId, CartDto cartDto) {
+
+    Cart cart = cartRepository
+        .findByCustomerId(customerId)
+        .orElseThrow(() -> new ResourceNotFoundException("Cart", "customerId", customerId.toString()));
+
+    Cart updatedCart = orderMapper.toCartEntity(cartDto);
+    updatedCart.setId(cart.getId());
+
+    Cart savedCart = cartRepository.save(updatedCart);
+    return orderMapper.toCartDto(savedCart);
+  }
 }
