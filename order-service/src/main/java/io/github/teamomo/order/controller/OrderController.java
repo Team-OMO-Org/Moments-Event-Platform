9b1db86cef55559f267b68e7a09e662c65c38573
OlenaShcherbina <olenas.shcherbina@gmail.com>
2025-05-19 14:29:36 +0200

    add endpoints for Cart and CartItem in OrderController

--- order-service/src/main/java/io/github/teamomo/order/controller/OrderController.java
+++ order-service/src/main/java/io/github/teamomo/order/controller/OrderController.java
@@ -100,6 +100,17 @@ public class OrderController {
     return cartDto;
   }
 
+  @PostMapping("/carts/{customerId}")
+  @ResponseStatus(HttpStatus.CREATED)
+  public CartDto createCart(@PathVariable Long customerId, @Valid @RequestBody CartDto cartDto) {
+
+    logger.info("Creating new cart with details: {}", cartDto);
+    CartDto createdCartDto = orderService.createCart(cartDto);
+    logger.info("Successfully created cart with ID: {}", createdCartDto.id());
+
+    return createdCartDto;
+  }
+
   @PutMapping("/carts/{customerId}")
   @ResponseStatus(HttpStatus.OK)
   public CartDto updateCart(@PathVariable Long customerId, @Valid @RequestBody CartDto cartDto) {
