94afed3acc3c613af9b1a0f0e8b40c67b16abfca
MichaelJG.Kopp <michaeljg.kopp@gmail.com>
2025-06-05 16:02:49 +0200

    add logout functionality and button to navbar

--- moments-web-app/src/main/java/io/github/teamomo/momentswebapp/config/SecurityConfig.java
+++ moments-web-app/src/main/java/io/github/teamomo/momentswebapp/config/SecurityConfig.java
@@ -2,20 +2,17 @@ package io.github.teamomo.momentswebapp.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.Ordered;
-import org.springframework.core.annotation.Order;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
-import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
-import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClientManager;
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClientProvider;
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClientProviderBuilder;
+import org.springframework.security.oauth2.client.oidc.web.logout.OidcClientInitiatedLogoutSuccessHandler;
 import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
 import org.springframework.security.oauth2.client.web.DefaultOAuth2AuthorizedClientManager;
 import org.springframework.security.oauth2.client.web.OAuth2AuthorizedClientRepository;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
 
 @Configuration
 public class SecurityConfig {
@@ -36,12 +33,17 @@ public class SecurityConfig {
 //  }
 
   @Bean
-  public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+  public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity,
+      LogoutSuccessHandler keycloakLogoutSuccessHandler) throws Exception {
     return httpSecurity
         .authorizeHttpRequests(authorize -> authorize
             .requestMatchers(freeResourceUrls).permitAll()
             .anyRequest().authenticated())
         .oauth2Login(Customizer.withDefaults())
+        .logout(logout -> logout
+            .logoutSuccessHandler(keycloakLogoutSuccessHandler)  // use custom handler
+            .permitAll()
+        )
         .build();
   }
 
@@ -61,5 +63,17 @@ public class SecurityConfig {
     authorizedClientManager.setAuthorizedClientProvider(authorizedClientProvider);
 
     return authorizedClientManager;
+  }
+
+  @Bean
+  public LogoutSuccessHandler keycloakLogoutSuccessHandler(
+      ClientRegistrationRepository repository) {
+
+    OidcClientInitiatedLogoutSuccessHandler logoutSuccessHandler =
+        new OidcClientInitiatedLogoutSuccessHandler(repository);
+
+    logoutSuccessHandler.setPostLogoutRedirectUri("{baseUrl}");
+
+    return logoutSuccessHandler;
   }
 }
